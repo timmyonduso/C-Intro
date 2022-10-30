9fffb75ece90f7d50001077c6f40123dee074918
timmzzyy <timmyonduso58@gmail.com>
2022-10-30 14:05:48 +0300

    luku

---
+++ Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Intro
+{
+    class Variable
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+
+            int myNum = 15;
+            Console.WriteLine(myNum);
+
+            int Admission;
+            Admission = 8081;
+            Console.WriteLine(Admission);
+
+            int younum = 8;
+            double rakid = 90.99D;
+            char grade = 'A';
+            Boolean decision = true;
+            string thisText = "Hi hello";
+
+
+            const int thisnum = 923;
+            // thisnum = 90; will generete error
+            //comment
+
+        }
+
+    }
+}
